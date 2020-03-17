e086a745c13e196400b6bd77d81e1837293de593
Salomao Rodrigues <salomao88@gmail.com>
2020-03-17 22:57:29 +0000

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import { buildSchema } from 'graphql'
+import cors from 'cors'
 
 const { connectDB, getDBClient, disconnectDB } = require('../src/dbClient')
 
@@ -58,6 +59,7 @@ const root = {
 }
 
 const app = express()
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,

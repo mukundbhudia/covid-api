cc33b7106d083ed90d45d198d65e45948e838b36
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-07-17 10:42:46 +0100

    Abstracted out db connection to one function

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,29 +1,30 @@
 const { connectDB, getDBClient, disconnectDB } = require('../src/dbClient')
 
+const loadDbAndClient = async () => {
+  await connectDB()
+  return getDBClient()
+}
+
 // The root provides a resolver function for each API endpoint
 const root = {
   globalTimeSeries: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     const { timeSeriesTotalCasesByDate } = await dbClient.collection('totals').findOne()
     return timeSeriesTotalCasesByDate
   },
   getAllCountries: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     const { allCountries } = await dbClient.collection('totals').findOne()
     return allCountries
   },
   getAllDaysWithCases: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     const { globalCasesByDate } = await dbClient.collection('totals').findOne()
     return globalCasesByDate.map((cases) => { return cases.day })
   },
   getGlobalCasesByDate: async (args) => {
     if (args && args.day) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const { globalCasesByDate } = await dbClient.collection('totals').findOne()
       let results = []
       globalCasesByDate.forEach(globalCase => {
@@ -37,24 +38,21 @@ const root = {
   },
   getCasesByIdKey: async (args) => {
     if (args && args.idKey) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({idKey: args.idKey})
       return await cursor.toArray()
     }
   },
   getManyCasesByIdKey: async (args) => {
     if (args && args.idKeys) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({idKey: { "$in" : args.idKeys}})
       return await cursor.toArray()
     }
   },
   getCasesWithCountry: async (args) => {
     if (args && args.country) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({country: args.country})
       return await cursor.toArray()
     }
@@ -64,8 +62,7 @@ const root = {
       if (args.province === '') {
         args.province = null
       }
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({
         country: args.country,
         province: args.province
@@ -75,82 +72,71 @@ const root = {
   },
   getProvincesGivenCountryName: async (args) => {
     if (args && args.country) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'hasProvince': false, country: args.country}).sort({'confirmed': -1})
       return await cursor.toArray()
     }
   },
   topXconfirmedByCountry: async (args) => {
     if (args && args.limit) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'province': null}).sort({'confirmed': -1}).limit(args.limit)
       return await cursor.toArray()
     }
   },
   topXactiveByCountry: async (args) => {
     if (args && args.limit) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'province': null}).sort({'active': -1}).limit(args.limit)
       return await cursor.toArray()
     }
   },
   topXrecoveredByCountry: async (args) => {
     if (args && args.limit) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'province': null}).sort({'recovered': -1}).limit(args.limit)
       return await cursor.toArray()
     }
   },
   topXdeathsByCountry: async (args) => {
     if (args && args.limit) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'province': null}).sort({'deaths': -1}).limit(args.limit)
       return await cursor.toArray()
     }
   },
   topXconfirmedTodayByCountry: async (args) => {
     if (args && args.limit) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'province': null}).sort({'confirmedCasesToday': -1}).limit(args.limit)
       return await cursor.toArray()
     }
   },
   topXdeathsTodayByCountry: async (args) => {
     if (args && args.limit) {
-			await connectDB()
-			const dbClient = getDBClient()
+      const dbClient = await loadDbAndClient()
       const cursor = await dbClient.collection('casesByLocation').find({'province': null}).sort({'deathsToday': -1}).limit(args.limit)
       return await cursor.toArray()
     }
   },
   lastUpdated: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     const { timeStamp } = await dbClient.collection('totals').findOne()
     return timeStamp
   },
   casesByLocation: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     const cursor = await dbClient.collection('casesByLocation').find({})
     return await cursor.toArray()
   },
   casesByLocationWithNoProvince: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     // We make an exception for Greenland as some datasets consider it to be it's on country
     const cursor = await dbClient.collection('casesByLocation').find( { $or: [ { 'province': null }, { 'province': 'Greenland' } ] } )
     return await cursor.toArray()
   },
   totalCases: async () => {
-		await connectDB()
-		const dbClient = getDBClient()
+    const dbClient = await loadDbAndClient()
     return await dbClient.collection('totals').findOne()
   },
 }
